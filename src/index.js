052d75550d2328bc532f87290203367a659e040f
Cris Ryan Tan <crisryantan@gmail.com>
2017-01-25 14:47:18 +0800

    added sample reducer

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()( createStore );
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={ createStoreWithMiddleware( reducers ) }>
+		<App />
+	</Provider>, document.querySelector('.container') );
