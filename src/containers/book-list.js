bcb73a05fdbf09af835affdab197f3f9b4001f64
Cris Ryan Tan <crisryantan@gmail.com>
2017-01-26 19:45:31 +0800

    add onclick when book is selected

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
 
 	renderList () {
 		return this.props.books.map( ( book ) => {
 			return (
-				<li key={ book.title } className="list-group-item">{ book.title }</li>
+				<li
+					key={ book.title }
+					className="list-group-item"
+					onClick={ () => this.props.selectBook( book ) }>{ book.title }</li>
 			)
 		} );
 	}
@@ -27,4 +32,15 @@ function mapStateToProps( state ) {
 	};
 }
 
-export default connect( mapStateToProps )( BookList );
+// anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps( dispatch ) {
+	// whenever selectBook is called, the result should be passed
+	// to all of our reducers
+	return bindActionCreators( { selectBook : selectBook }, dispatch );
+}
+
+// Promote BookList from a component to a container
+// to know about the new dispatch method, selectBook to make
+// it available as a prop
+export default connect( mapStateToProps, mapDispatchToProps )( BookList );
